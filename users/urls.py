8b1e84bfac23a45ba4c2b033ec76598718a9ea1a
Noheemot <holarkunbi@gmail.com>
2020-05-18 14:38:39 +0100

    remove success url from urls.py

--- users/urls.py
+++ users/urls.py
@@ -11,9 +11,7 @@ urlpatterns = [
     path('registration/', registrationview, name='registration'),
     path('login/', loginview, name='loginpage'),
 
-    path('password_reset/', 
-    auth_views.PasswordResetView.as_view(success_url = reverse_lazy('user:password_reset_done')), 
-    name='password_reset'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
 
     path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(), name ='password_reset_done'),
 
